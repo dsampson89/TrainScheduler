b3da1252f529b51b43788282f1f5d35ac64df56e
Dexter Sampson <dexter1.sampson@gmail.com>
2020-03-18 12:00:03 -0400

    database issue

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,15 +1,46 @@
 // Initialize Firebase
-var firebaseConfig = {
-    apiKey: "AIzaSyBRk2cmsnF5C6QRUizqTtsky0BypxjjcjI",
-    authDomain: "newproject-3e5da.firebaseapp.com",
-    databaseURL: "https://newproject-3e5da.firebaseio.com",
-    projectId: "newproject-3e5da",
-    storageBucket: "newproject-3e5da.appspot.com"
+var config = {
+    apiKey: "AIzaSyAYSY8Yufzd3WvCiQlwb5bgqZ3EmX-QodM",
+    authDomain: "train-schedule-219e2.firebaseapp.com",
+    databaseURL: "https://train-schedule-219e2.firebaseio.com",
+    projectId: "train-schedule-219e2",
+    storageBucket: "train-schedule-219e2.appspot.com"
 };
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config);
 
 var database = firebase.database();
 
+//Adding Train
+$("#add-train-btn").on("click", function(event){
+    //Prevent Clearing Data
+    event.preventDefault();
 
+    //Input data
+    txName = $("#train-name-input").val().trim();
+    txDestination = $("#destination-input").val().trim();
+    txTime = $("#train-time-input").val().trim();
+    txFrequency = $("#frequency-input").val().trim();
+
+    // Local variable
+    var newTx = {
+        name: txName,
+        destination: txDestination,
+        time: txTime,
+        frequency: txFrequency,
+    };
+
+    //Push new data
+    database.ref().push(newTx);
+
+    //Log new data
+    console.log(newTx.name);
+    console.log(newTx.destination);
+    console.log(newTx.time);
+    console.log(newTx.frequency);
+
+
+
+
+})
 
