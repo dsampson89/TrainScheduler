97fb821d97762b631e1c8c2482e86efbb13a9745
Dexter Sampson <dexter1.sampson@gmail.com>
2020-03-18 10:02:53 -0400

    initilize firebase

---
+++ assets/javascript/train.js
@@ -0,0 +1,15 @@
+// Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyBRk2cmsnF5C6QRUizqTtsky0BypxjjcjI",
+    authDomain: "newproject-3e5da.firebaseapp.com",
+    databaseURL: "https://newproject-3e5da.firebaseio.com",
+    projectId: "newproject-3e5da",
+    storageBucket: "newproject-3e5da.appspot.com"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+
+
