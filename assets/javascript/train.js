5e3d2e1e6cef914a1bf77f07f1f347910ebdcd0b
Dexter Sampson <dexter1.sampson@gmail.com>
2020-03-22 18:38:23 -0400

    math and new rows

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -61,7 +61,33 @@ database.ref().on("child_added", function(childSnapshot) {
     console.log(txTime);
     console.log(txFrequency);
 
-    var 
+    //covert to military time
+    var firstTrainCovereted = moment(txTime, "HH:mm").subtract(1, "years");
+
+    // difference between current time and first train time
+    var difTime = moment().diff(moment(firstTrainCovereted), "minutes");
+
+    //time apart
+    var tRemainder = difTime % txFrequency;
+
+    //minutes till next train
+    var minTillTx = txFrequency - tRemainder;
+
+    // result
+    var nxtTx = moment().add(minTillTx, "minutes");
+
+    //adding new rows
+    var newRow = $("<tr>").append(
+        $("<td>").text(txName),
+        $("<td>").text(txDestination),
+        $("<td>").text(txFrequency),
+        $("<td>").text(nxtTx),
+        $("<td>").text(minTillTx)
+    );
+
+        //appending new rows
+    $("#train-table > tbody").append(newRow);
+
 
 
 });
